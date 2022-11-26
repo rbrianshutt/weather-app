12fc7cfe86a03850301ed1046a94db667a0e5b12
rbrianshutt <rbrianshutt@gmai.com>
2022-11-26 15:53:17 -0600

    update

---
+++ components/Spinner.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import spinner from '../public/spinner.gif';
+import Image from 'next/image';
+
+function Spinner() {
+	return (
+		<div>
+			<Image className='w-[200px] m-auto block' src={spinner} alt='loading..' />
+		</div>
+	);
+}
+
+export default Spinner;
